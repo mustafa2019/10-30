ac5bbee0357aa5977cdc313c3d637ae296dc2697
ADMIN <ADMIN@192.168.1.11>
2018-02-21 20:19:07 +0530

    new build

---
+++ org.guru.bank/src/test/java/org/guru/bank/Test_001.java
@@ -0,0 +1,12 @@
+package org.guru.bank;
+
+import org.testng.annotations.Test;
+
+public class Test_001 {
+	@Test
+	public void chk()
+	{
+		System.out.println("Welcome to the Maven World");
+	}
+
+}
